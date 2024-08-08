761c49020079dd0cbd4f1abe674f810343e472f7
naetharu <naetharu@gmail.com>
2024-08-08 18:06:55 +0100

    add custom drop down menu to the app

---
+++ kanban-ui/src/components/DownDown/DropDown.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import ChevronArrow from "../Icons/ChevronArrow";
+import styles from "./DropDown.module.css";
+
+interface DropDownProps {
+  values: string[];
+  value: string;
+  setValue: (value: string) => void;
+}
+
+const DropDown = ({ values, value, setValue }: DropDownProps) => {
+  const [showOptions, setShowOptions] = useState<boolean>(false);
+
+  const handleToggleOptions = () => {
+    setShowOptions(!showOptions);
+  };
+
+  return (
+    <div className={styles.container}>
+      <button
+        className={`${styles.button} ${
+          showOptions && styles.buttonOpen
+        } text-lg `}
+        onClick={handleToggleOptions}
+      >
+        {value ? value : "Choose an Option"}
+        <ChevronArrow />
+        {showOptions && (
+          <div className={`${styles.optionsBox} text-lg`}>
+            {values.map((option) => (
+              <div key={option} onClick={() => setValue(option)}>
+                {option}
+              </div>
+            ))}
+          </div>
+        )}
+      </button>
+    </div>
+  );
+};
+
+export default DropDown;

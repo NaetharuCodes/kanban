b673675f39d0099e8ff89b57cca4ac87cf337438
naetharu <naetharu@gmail.com>
2024-08-12 08:08:49 +0100

    Align text in new cols container

--- kanban-ui/src/App.tsx
+++ kanban-ui/src/App.tsx
@@ -9,17 +9,33 @@ const App = () => {
 
   return (
     <AppShell>
-      <div className={styles.mainContainer}>
-        {dummyData.colData.map((col) => (
-          <Column
-            key={col.colId}
-            colId={col.colId}
-            colName={col.colName}
-            colColor={col.colColor}
-            colItems={col.colItems}
-          />
-        ))}
-      </div>
+      {!dummyData.colData ? (
+        <div className={styles.mainContainer}>
+          {dummyData.colData.map((col) => (
+            <Column
+              key={col.colId}
+              colId={col.colId}
+              colName={col.colName}
+              colColor={col.colColor}
+              colItems={col.colItems}
+            />
+          ))}
+          <div className={styles.newColContainer}>
+            <button className={`${styles.newColBtn} heading-xl`}>
+              + New Column
+            </button>
+          </div>
+        </div>
+      ) : (
+        <div className={styles.noColsContainer}>
+          <p className={`${styles.noColsText} heading-lg`}>
+            This board is empty. Createa a new column to get started.
+          </p>
+          <button className={`${styles.noColsBtn} heading-md`}>
+            + Add New Column
+          </button>
+        </div>
+      )}
     </AppShell>
   );
 };

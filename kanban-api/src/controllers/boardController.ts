1ebf9363479334b41f279802159823bfbaafaae9
naetharu <naetharu@gmail.com>
2024-08-14 19:21:29 +0100

    add create board

--- kanban-api/src/controllers/boardController.ts
+++ kanban-api/src/controllers/boardController.ts
@@ -1,25 +1,34 @@
-import {Request, Response} from 'express';
+import { PrismaClient } from "@prisma/client";
+import { Request, Response } from "express";
+
+const prisma = new PrismaClient();
 
 export const getAllBoards = (req: Request, res: Response) => {
-    res.send('get all boards');    
-}
+  res.send("get all boards");
+};
 
 export const getBoard = (req: Request, res: Response) => {
-    res.send('get specific board by id');    
-}
+  res.send("get specific board by id");
+};
 
-export const createBoard = (req: Request, res: Response) => {
-    const {name} = req.body
-    console.log('reqesting', name)
-
-
-    
-}
+export const createBoard = async (req: Request, res: Response) => {
+  try {
+    const { name } = req.body;
+    const newBoard = await prisma.board.create({
+      data: {
+        name: name,
+      },
+    });
+    res.json(newBoard);
+  } catch (error) {
+    res.status(500).json({ error: "unable to create a new board" });
+  }
+};
 
 export const updateBoard = (req: Request, res: Response) => {
-    res.send('update an exisitng board');    
-}
+  res.send("update an exisitng board");
+};
 
 export const deleteBoard = (req: Request, res: Response) => {
-    res.send('delete a board');    
-}
+  res.send("delete a board");
+};

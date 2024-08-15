b563184ac7d862a21a7e9e0c41d5877e4404a62d
naetharu <naetharu@gmail.com>
2024-08-15 18:58:30 +0100

    Create controller logic for get cols and create cols

--- kanban-api/src/controllers/colController.ts
+++ kanban-api/src/controllers/colController.ts
@@ -4,11 +4,33 @@ import { Request, Response } from "express";
 const prisma = new PrismaClient();
 
 export const getCols = async (req: Request, res: Response) => {
-  res.send("Get All Cols");
+  try {
+    const { boardId } = req.body;
+    const boardCols = await prisma.col.findMany({
+      where: {
+        boardId: boardId,
+      },
+    });
+    res.status(200).json(boardCols);
+  } catch (error) {
+    res.status(500).json({ error: "failed to get cols" });
+  }
 };
 
 export const createCol = async (req: Request, res: Response) => {
-  res.send("Create a new col");
+  try {
+    const { boardId, name, color } = req.body;
+    const newCol = await prisma.col.create({
+      data: {
+        boardId: parseInt(boardId),
+        name: name as string,
+        color: color as string,
+      },
+    });
+    res.status(200).json(newCol);
+  } catch (error) {
+    res.status(500).json({ error: "failed to create a new col" });
+  }
 };
 
 export const updateCol = async (req: Request, res: Response) => {
